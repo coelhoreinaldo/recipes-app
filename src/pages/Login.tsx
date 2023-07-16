08db7816759d288d364feffd517d937de3975f92
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-16 08:47:50 -0300

    :sparkles: feat: handleChange inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [userData, setUserData] = useState({
+    emailInput: '',
+    passwordInput: '',
+  });
+
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
   return (
     <form>
       <label htmlFor="password-input">
         <input
           type="email"
-          name="email-input"
+          name="emailInput"
           id="email-input"
           data-testid="email-input"
+          onChange={ handleChange }
         />
       </label>
 
       <label htmlFor="password-input">
         <input
           type="password"
-          name="password-input"
+          name="passwordInput"
           id="password-input"
           data-testid="password-input"
+          onChange={ handleChange }
         />
       </label>
 
