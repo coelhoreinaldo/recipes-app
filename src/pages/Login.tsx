5dd62b0c835c9f5709e3d4d822da28298a8a7912
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-16 09:02:34 -0300

    :sparkles: feat: validateLoginInputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Login() {
   const [userData, setUserData] = useState({
     emailInput: '',
     passwordInput: '',
   });
+  const { emailInput, passwordInput } = userData;
+  const [invalidInputs, setInvalidInputs] = useState(true);
 
   const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = target;
-
     setUserData({
       ...userData,
       [name]: value,
     });
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput) || passwordInput.length <= 6) {
+      return setInvalidInputs(true);
+    }
+    return setInvalidInputs(false);
+  }, [emailInput, passwordInput]);
+
   return (
-    <form className="flex flex-col items-center bg-red-400 w-64 gap-4">
+    <form
+      onSubmit={ handleSubmit }
+      className="flex flex-col items-center bg-red-400 w-64 gap-4"
+    >
       <label htmlFor="password-input" className="flex flex-col">
         Email
         <input
@@ -42,7 +58,8 @@ export default function Login() {
       <button
         type="submit"
         data-testid="login-submit-btn"
-        className="bg-blue-400 border-lime-400 border-4"
+        className="bg-blue-400 border-lime-400 border-4 disabled:bg-gray-200"
+        disabled={ invalidInputs }
       >
         Enter
       </button>
