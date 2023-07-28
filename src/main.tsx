8100a5b7b87cba9020c87cd39ecd71b697650fbf
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-28 12:22:42 -0300

    🧱 build: ghpages

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import Provider from './context/Provider';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    <BrowserRouter>
+    <HashRouter>
       <Provider>
         <App />
       </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
   );
